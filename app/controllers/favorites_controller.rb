961147c0d4a54ea015cdb1fd0740a66214d1118c
Kasai-Kouki <kko_7230@yahoo.co.jp>
2019-11-15 03:36:00 +0000

    rails6

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,22 @@
+class FavoritesController < ApplicationController
+  def index
+    @favorite_topics = current_user.favorite_topics
+  end
+
+  def create
+    favorite = Favorite.new
+    favorite.user_id = current_user.id
+    favorite.topic_id = params[:topic_id]
+
+    if favorite.save
+      redirect_to topics_path, success: 'お気に入りに登録しました'
+    else
+      redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
+    end
+  def show
+    @post = Post.find_by(id: params[:id])
+    @user = @post.user
+    @likes_count = Like.where(post_id: @post.id).count
+  end
+  end
+end
